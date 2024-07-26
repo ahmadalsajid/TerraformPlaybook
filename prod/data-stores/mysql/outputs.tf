04eaed43c16747d73e7f07225d99521eb1d2cb35
ahmadalsajid <ahmadalsajid@gmail.com>
2024-07-26 12:58:57 +0600

    iterarion of count index as for loop

--- prod/data-stores/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value = aws_db_instance.example.address
-  description = "database connection endpoint"
-}
-
-output "port" {
-  value = aws_db_instance.example.port
-  description = "database connection port"
-}
