6f060e8bb60045c6635f68ac24e8047bee1b84d3
ahmadalsajid <ahmadalsajid@gmail.com>
2024-07-26 16:22:54 +0600

    for loop in looping map and list

--- live/global/iam/outputs.tf
+++ live/global/iam/outputs.tf
@@ -5,3 +5,11 @@ output "upper_names" {
 output "upper_names_with_condition" {
   value = [for name in var.user_names : upper(name) if length(name) < 6]
 }
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
--- live/global/iam/variables.tf
+++ live/global/iam/variables.tf
@@ -3,3 +3,13 @@ variable "user_names" {
   type        = list(string)
   default     = ["ahmad", "sajid", "tasmiah"]
 }
+
+variable "hero_thousand_faces" {
+  description = "map"
+  type        = map(string)
+  default = {
+    neo      = "hero"
+    trinity  = "love interest"
+    morpheus = "mentor"
+  }
+}
